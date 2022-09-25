b55e0bf6943be5e90a13d69df4b39377a75102d3
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-09-25 09:30:06 +0900

    フロントエンドとバックエンドをつなぐ

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,11 @@
 import { Fragment } from "react";
+import React, { useEffect } from "react";
+import { fetchRestaurants } from "../apis/restaurants";
 
 export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants().then((data) => console.log(data));
+  }, []);
+
   return <Fragment>レストラン一覧</Fragment>;
 };
