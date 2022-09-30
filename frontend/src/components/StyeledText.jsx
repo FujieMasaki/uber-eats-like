4b32d1943ef221450b63ac5651167e733342e7a4
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-09-30 18:26:37 +0900

    food一覧ページを追加

---
+++ frontend/src/components/StyeledText.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { COLORS, FONT_SIZE, FRONT_SIZE } from "../styled.constants";
+
+export const SubText = styled.p`
+  color: ${COLORS.SUB_TEXT};
+  font-size: ${FONT_SIZE.BODY2};
+`;
