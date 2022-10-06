44767bc6122fb108fb4f460311fea7b5e8f0a239
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2022-10-07 07:12:02 +0900

    サーバーサイドをリファクタリング

--- db/migrate/20220922094125_create_restaurants.rb
+++ db/migrate/20220922094125_create_restaurants.rb
@@ -1,8 +1,8 @@
 class CreateRestaurants < ActiveRecord::Migration[6.0]
   def change
     create_table :restaurants do |t|
-      t.string :name, null: false
-      t.integer :fee, null: false, default: 0
+      t.string :name, null: false, comment:"レストランの名前"
+      t.integer :fee, null: false, default: 0, comment:"価格"
       t.integer :time_required, null: false
 
       t.timestamps
